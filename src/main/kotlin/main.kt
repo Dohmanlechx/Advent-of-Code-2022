f7767228b4a21e6d20e4d8a6c080ca7734a4b97a
David Öhman <dohman_92@hotmail.com>
2022-12-05 10:56:10 +0100

    Solve Day 5 P1 P2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,17 +2,26 @@ import puzzles.Day01
 import puzzles.Day02
 import puzzles.Day03
 import puzzles.Day04
+import puzzles.Day05
 
 fun main() {
     Day01().p1()
     Day01().p2()
+    println("Day 1 done!")
 
     Day02().p1()
     Day02().p2()
+    println("Day 2 done!")
 
     Day03().p1()
     Day03().p2()
+    println("Day 3 done!")
 
     Day04().p1()
     Day04().p2()
+    println("Day 4 done!")
+
+    Day05().p1()
+    Day05().p2()
+    println("Day 5 done!")
 }
---
+++ src/main/kotlin/puzzles/Day05.kt
@@ -0,0 +1,72 @@
+package puzzles
+
+import reader.Reader
+
+typealias Stack = MutableList<Char>
+
+class Day05 {
+    private val instructions =
+        Reader.input(5, split = "\n\n")[1]
+            .filter { it.isDigit() || it.isWhitespace() }
+            .split("\n")
+            .map(String::trim)
+
+    private val crates: Map<Int, Stack> = mapOf(
+        1 to mutableListOf('B', 'S', 'V', 'Z', 'G', 'P', 'W'),
+        2 to mutableListOf('J', 'V', 'B', 'C', 'Z', 'F'),
+        3 to mutableListOf('V', 'L', 'M', 'H', 'N', 'Z', 'D', 'C'),
+        4 to mutableListOf('L', 'D', 'M', 'Z', 'P', 'F', 'J', 'B'),
+        5 to mutableListOf('V', 'F', 'C', 'G', 'J', 'B', 'Q', 'H'),
+        6 to mutableListOf('G', 'F', 'Q', 'T', 'S', 'L', 'B'),
+        7 to mutableListOf('L', 'G', 'C', 'Z', 'V'),
+        8 to mutableListOf('N', 'L', 'G'),
+        9 to mutableListOf('J', 'F', 'H', 'C')
+    )
+
+    fun p1() {
+        for (instruction in instructions) {
+            instruction.execute { count, from, to ->
+                val a = crates.getValue(from)
+                val b = crates.getValue(to)
+                repeat(count) { a oneCrateTo b }
+            }
+        }
+
+        val res = crates.values.tops()
+        require(res == "VJSFHWGFT")
+    }
+
+    fun p2() {
+        for (instruction in instructions) {
+            instruction.execute { count, from, to ->
+                val a = crates.getValue(from)
+                val b = crates.getValue(to)
+                a.cratesTo(b, count)
+            }
+
+        }
+
+        val res = crates.values.tops()
+        require(res == "LCTQFBVZV")
+    }
+
+    private fun String.execute(action: (Int, Int, Int) -> Unit) {
+        val digits = split("  ").map(String::toInt)
+        action(digits[0], digits[1], digits[2])
+    }
+
+    private infix fun Stack.oneCrateTo(other: Stack) {
+        val item = removeLast()
+        other.add(item)
+    }
+
+    private fun Stack.cratesTo(other: Stack, count: Int) {
+        val items = mutableListOf<Char>()
+        repeat(count) { items.add(removeLast()) }
+        other.addAll(items.reversed())
+    }
+
+    private fun Collection<Stack>.tops(): String =
+        map(Stack::last).joinToString("")
+
+}
